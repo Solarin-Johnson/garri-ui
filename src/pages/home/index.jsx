e1c18f57a4d7da47dcc7b4eb8bee7d4ce25f8d12
Solarin-Johnson <solarinjohnson@gmail.com>
2024-05-14 00:23:49 +0100

    chore: Update styles for home page
    - Adjust image sizes and margins in home.scss
    - Update font size and alignment in home.scss
    - Add conditional logic to handle mobile devices in index.jsx
    - Modify speed of parallax layers based on device type in index.jsx
    - Remove unnecessary comments and unused code

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -10,9 +10,11 @@ import image3 from "../../images/Garri-legacy-2.png";
 export default function Home({ pageIndex }) {
   const parallax = useRef(null);
   const [index, setIndex] = useState(0);
+  const [isMobile, setIsMobile] = useState(isMobileDevice());
+
   useEffect(() => {
     const handleResize = () => {
-      parallax && parallax.current.scrollTo(index);
+      parallax && !isMobile && parallax.current.scrollTo(index);
     };
 
     window.addEventListener("resize", handleResize);
@@ -24,8 +26,19 @@ export default function Home({ pageIndex }) {
 
   useEffect(() => {
     setIndex(pageIndex);
-    parallax && parallax.current.scrollTo(pageIndex);
-  }, [pageIndex]);
+    parallax && !isMobile && parallax.current.scrollTo(pageIndex);
+  }, [pageIndex, isMobile]);
+
+  function isMobileDevice() {
+    return (
+      typeof window.orientation !== "undefined" ||
+      navigator.userAgent.indexOf("IEMobile") !== -1
+    );
+  }
+
+  const speed = (x) => {
+    return isMobile ? x : x;
+  };
 
   return (
     <div className="home-container">
@@ -36,85 +49,76 @@ export default function Home({ pageIndex }) {
       >
         <ParallaxLayer
           offset={1}
-          speed={1}
+          speed={speed(1)}
           factor={1.3}
           style={{ backgroundColor: "var(--color-1)" }}
         />
         <ParallaxLayer
           offset={2}
-          speed={1}
+          speed={speed(1)}
           factor={4}
           style={{ backgroundColor: "var(--color-2)" }}
         />
         <ParallaxLayer
-          offset={0}
-          speed={0}
-          factor={4}
-          style={{
-            backgroundImage: `url(${bgImage})`,
-            backgroundSize: "cover",
-            backgroundPosition: "50%",
-            opacity: 0.8,
-          }}
-        />
-
-        {/* Home */}
-        <ParallaxLayer offset={0} speed={0.15} className="home-image">
-          <img src={image1} alt="" />
-        </ParallaxLayer>
-        <ParallaxLayer offset={0} speed={-0.07} className="home-title">
-          <div>Discover the Flavourful World of Garri!</div>
-        </ParallaxLayer>
-
-        {/* Slow Bg */}
-        <ParallaxLayer
-          offset={0}
-          speed={0.4}
-          factor={4}
-          style={{
-            backgroundImage: `url(${bgImageDraw})`,
-            backgroundSize: "cover",
-            backgroundPosition: "40%",
-            opacity: 0.85,
-          }}
-        />
-        {/* Second Page */}
-        <ParallaxLayer
           offset={1}
-          speed={-0.07}
+          speed={speed(-0.07)}
           className="home-title home-title-2"
         >
           <div>Dive into Tradition with Garri and Groundnut!</div>
         </ParallaxLayer>
         <ParallaxLayer
+          offset={0}
+          speed={speed(0)}
+          factor={4}
+          style={{
+            backgroundImage: !isMobile && `url(${bgImage})`,
+            backgroundSize: "cover",
+            backgroundPosition: "50%",
+            opacity: 0.8,
+          }}
+        />
+        <ParallaxLayer offset={0} speed={speed(0.15)} className="home-image">
+          <img src={image1} alt="" />
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={speed(-0.07)} className="home-title">
+          <div>Discover the Flavourful World of Garri!</div>
+        </ParallaxLayer>
+        <ParallaxLayer
           offset={1}
-          speed={0.15}
+          speed={speed(0.15)}
           className="home-image home-image-2"
         >
           <img src={image2} alt="" />
         </ParallaxLayer>
-
-        {/* Third Page */}
         <ParallaxLayer
           offset={2}
-          speed={-0.07}
+          speed={speed(-0.07)}
           className="home-title home-title-3"
         >
           <div>Taste the Tradition Today!</div>
         </ParallaxLayer>
         <ParallaxLayer
+          offset={0}
+          speed={speed(0.4)}
+          factor={4}
+          style={{
+            backgroundImage: `url(${bgImageDraw})`,
+            backgroundSize: "cover",
+            backgroundPosition: !isMobile ? "40%" : "center",
+            opacity: 0.85,
+          }}
+        />
+        <ParallaxLayer
           offset={2}
-          speed={0.15}
+          speed={speed(0.15)}
           className="home-image home-image-2"
         >
           <img src={image3} alt="" />
         </ParallaxLayer>
-
-        {/* Buttons */}
         <ParallaxLayer
           style={{ display: "flex" }}
           onClick={() => {
-            parallax.current.scrollTo(1);
+            !isMobile && parallax.current.scrollTo(1);
             setIndex(1);
           }}
         ></ParallaxLayer>
@@ -122,7 +126,7 @@ export default function Home({ pageIndex }) {
           offset={1}
           style={{ display: "flex" }}
           onClick={() => {
-            parallax.current.scrollTo(2);
+            !isMobile && parallax.current.scrollTo(2);
             setIndex(2);
           }}
         ></ParallaxLayer>
@@ -130,7 +134,7 @@ export default function Home({ pageIndex }) {
           offset={2}
           style={{ display: "flex" }}
           onClick={() => {
-            parallax.current.scrollTo(0);
+            !isMobile && parallax.current.scrollTo(0);
             setIndex(0);
           }}
         ></ParallaxLayer>
